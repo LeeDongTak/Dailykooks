a20ac0e7c724e548e1e544fae26e597538001a75
LeeDongTak <sunny5796@naver.com>
2023-12-06 20:48:44 +0900

    마커 여러개 띄우기 완성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,55 +1,77 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Map, MapMarker, useKakaoLoader } from 'react-kakao-maps-sdk';
+import { Cheerio } from 'cheerio';
 
 import styled from 'styled-components';
 
 function Home() {
   const { kakao } = window;
-  const [state, setState] = useState({
-    // 지도의 초기 위치
-    center: { lat: 37.49676871972202, lng: 127.02474726969814 },
-    // 지도 위치 변경시 panto를 이용할지(부드럽게 이동)
-    isPanto: true
-  });
+  const mapRef = useRef(null);
   const [searchAddress, SetSearchAddress] = useState();
+  const [markers, setMarkers] = useState([]);
 
   // 키워드 입력후 검색 클릭 시 원하는 키워드의 주소로 이동
   const SearchMap = () => {
     const ps = new kakao.maps.services.Places();
+
     const placesSearchCB = function (data, status, pagination) {
       if (status === kakao.maps.services.Status.OK) {
         const newSearch = data[0];
-        setState({
-          center: { lat: newSearch.y, lng: newSearch.x }
-        });
+        setMarkers([...data.map((x) => x)]);
+        // setMarkers(newSearch);
         console.log(newSearch);
+        console.log(data);
       }
     };
     ps.keywordSearch(`${searchAddress}`, placesSearchCB);
   };
-
+  
   const handleSearchAddress = (e) => {
     SetSearchAddress(e.target.value);
   };
 
+  const bounds = useMemo(() => {}, [markers]);
+  useEffect(() => {
+    const bounds = new kakao.maps.LatLngBounds();
+    console.log(markers);
+    markers.forEach((point) => {
+      bounds.extend(new kakao.maps.LatLng(point.y, point.x));
+    });
+    const map = mapRef.current;
+    if (map) map.setBounds(bounds);
+  }, [markers]);
+
+  
   return (
     <>
       <Map // 지도를 표시할 Container
-        center={state.center}
-        isPanto={state.isPanto}
+        center={{
+          lat: 33.450701,
+          lng: 126.570667
+        }}
+        isPanto={true}
         style={{
           // 지도의 크기
           width: '100%',
           height: '450px'
         }}
+        ref={mapRef}
         level={3} // 지도의 확대 레벨
       >
-        <MapMarker position={state.center}> </MapMarker>
+        {markers.map((item) => (
+          <MapMarker key={`${item.x}-${item.y}`} position={{ lat: item.y, lng: item.x }} />
+        ))}
       </Map>
       <div>
         <input onChange={handleSearchAddress}></input>
-        <button onClick={SearchMap}>클릭</button>
+        <button
+          onClick={() => {
+            SearchMap();
+          }}
+        >
+          클릭
+        </button>
       </div>
     </>
   );
@@ -61,3 +83,6 @@ const StMap = styled.div`
   width: 500px;
   height: 400px;
 `;
+
+
+
